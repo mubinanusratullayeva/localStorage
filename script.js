c36828f7096547f2e2da221d36671d532dddd7cf
mubinanusratullayeva <mubinanusratullayeva1.@gmail.com>
2023-06-07 03:59:10 -0700

    fetch

--- script.js
+++ script.js
@@ -1,70 +1,100 @@
-const body = document.querySelector('body')
-      elInput = document.querySelector('.toDoInput')
-      modeBtn = document.querySelector('.ModeBtn')
-      toDoBtn = document.querySelector('.toDoBtn')
-      elUl = document.querySelector('.wrapper')
-      clearBtn = document.querySelector('.clearBtn')
+//LOCALSTORAGE
+
+// const body = document.querySelector('body')
+//       elInput = document.querySelector('.toDoInput')
+//       modeBtn = document.querySelector('.ModeBtn')
+//       toDoBtn = document.querySelector('.toDoBtn')
+//       elUl = document.querySelector('.wrapper')
+//       clearBtn = document.querySelector('.clearBtn')
 
 
-let themeStatus = localStorage.getItem('theme')
+// let themeStatus = localStorage.getItem('theme')
 
-if (themeStatus !== "dark") {
-      body.classList.remove('dark')
-}else{
-      body.classList.add('dark')
-}
+// if (themeStatus !== "dark") {
+//       body.classList.remove('dark')
+// }else{
+//       body.classList.add('dark')
+// }
 
-modeBtn.addEventListener('click', () => {
-      let theme = localStorage.getItem('theme')
-      if(theme === 'dark'){
-            body.classList.remove('dark');
-            localStorage.setItem('theme', 'light')
-      }else{
-            body.classList.toggle('dark')
-            localStorage.setItem('theme', 'dark')
-      }
+// modeBtn.addEventListener('click', () => {
+//       let theme = localStorage.getItem('theme')
+//       if(theme === 'dark'){
+//             body.classList.remove('dark');
+//             localStorage.setItem('theme', 'light')
+//       }else{
+//             body.classList.toggle('dark')
+//             localStorage.setItem('theme', 'dark')
+//       }
      
-})
+// })
 
 
-toDoBtn.addEventListener('click', ()=> {
+// toDoBtn.addEventListener('click', ()=> {
 
-      let valueIn = elInput.value;
+//       let valueIn = elInput.value;
 
-      function name(params) {
+//       function name(params) {
 
-            if (valueIn.trim() === '') {
-                  return
-            }
+//             if (valueIn.trim() === '') {
+//                   return
+//             }
 
-        let elLi = document.createElement('li');
-        elLi.textContent = valueIn;
-        let delBtn = document.createElement('button');
+//         let elLi = document.createElement('li');
+//         elLi.textContent = valueIn;
+//         let delBtn = document.createElement('button');
 
-        let smth = localStorage.getItem('smth')
+//         let smth = localStorage.getItem('smth')
 
-        if(smth){
-              elLi.textContent = valueIn
-        }
+//         if(smth){
+//               elLi.textContent = valueIn
+//         }
 
-      localStorage.setItem('smth', valueIn)
+//       localStorage.setItem('smth', valueIn)
 
-        delBtn.textContent = 'Delete';
-        delBtn.classList.add('delBtn');
+//         delBtn.textContent = 'Delete';
+//         delBtn.classList.add('delBtn');
     
-        delBtn.addEventListener('click', ()=> {
-          elLi.remove();
-        });
+//         delBtn.addEventListener('click', ()=> {
+//           elLi.remove();
+//         });
     
-        elLi.append(delBtn);
-        params.append(elLi);
+//         elLi.append(delBtn);
+//         params.append(elLi);
 
-      }
-      name(elUl);
+//       }
+//       name(elUl);
     
-      elInput.value = '';
+//       elInput.value = '';
+//     });
+
+//     clearBtn.addEventListener('click', ()=>{
+//       localStorage.removeItem('smth')
+//     })
+
+
+
+
+//JSONPLACEHOLDER
+
+
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(res => res.json())
+.then((data)=> {
+    // console.log(data);
+    data.forEach(item => {
+    let usersUl = document.querySelector('.users')
+    let text = document.querySelector('.text')
+    text.innerHTML = `name: ${item.name}<br>
+    username: ${item.username}<br>
+    email: ${item.email}`
+        let liEl = document.createElement('li')
+        liEl.classList.add('card')
+        liEl.innerHTML = `name: ${item.name}<br>
+        username: ${item.username}<br>
+        email: ${item.email}`
+        liEl.classList.add('btn')
+        liEl.setAttribute('data-bs-target', '#exampleModalToggle')
+        liEl.setAttribute('data-bs-toggle', 'modal')
+        usersUl.append(liEl)
     });
-
-    clearBtn.addEventListener('click', ()=>{
-      localStorage.removeItem('smth')
-    })
+}).catch(err => console.error(err))
